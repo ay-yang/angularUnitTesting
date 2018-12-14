63f3e424428efee2f90e5d515cab9209a6bf347a
Yue Yang <34326968+ay-yang@users.noreply.github.com>
2018-12-14 15:13:14 -0500

    corrected

--- src/app/dashboard/dashboard-hero.component.ts
+++ src/app/dashboard/dashboard-hero.component.ts
@@ -13,6 +13,6 @@ import { Hero } from '../model/hero';
 export class DashboardHeroComponent {
   @Input() hero: Hero;
   @Output() selected = new EventEmitter<Hero>();
-  click() { // this.selected.emit(this.hero);
+  click() { this.selected.emit(this.hero);
   }
 }
